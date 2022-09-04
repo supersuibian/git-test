df92213907ec5c296d2ac5e6b8ec884ab7055a1d
YuFeng <YuFeng@yf.com>
2022-09-04 17:54:43 +0800

    first commit

---
+++ src/main/java/com/yufeng/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.yufeng.git;
+
+/**
+ * @Author yf
+ * @Version 0.1
+ * @Description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
