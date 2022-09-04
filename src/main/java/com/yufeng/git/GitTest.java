a5113648c58de62e934a9b1cbd71c4115509a2dd
YuFeng <YuFeng@yf.com>
2022-09-04 18:32:39 +0800

    master commit

--- src/main/java/com/yufeng/git/GitTest.java
+++ src/main/java/com/yufeng/git/GitTest.java
@@ -8,6 +8,5 @@ public class GitTest {
         System.out.println("hello,git!");
         System.out.println("hello,git2!");
         System.out.println("master");
-        System.out.println("hot-fix");
     }
 }
