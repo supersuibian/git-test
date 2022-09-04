7a2023e79caa5654211df01c6f68ffa62dc7ba99
YuFeng <YuFeng@yf.com>
2022-09-04 18:56:53 +0800

    gitee commit

--- src/main/java/com/yufeng/git/GitTest.java
+++ src/main/java/com/yufeng/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello,git2!");
         System.out.println("master");
         System.out.println("pull");
+        System.out.println("gitee");
     }
 }
